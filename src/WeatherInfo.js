3c389fb4f26483e1edcaf1cc82713c2555797152
dvoritK <101967699+dvoritK@users.noreply.github.com>
2022-05-08 20:55:38 +0300

    Celsius to Fahrenheit Convertion

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
   return (
@@ -20,9 +21,7 @@ export default function WeatherInfo(props) {
             alt={props.data.description}
             className="icon"
           />
-
-          <span className="current-temperature">{props.data.temperature}</span>
-          <span className="celsius">°C</span>
+          <WeatherTemperature celsius={props.data.temperature} />
         </div>
 
         <div className="col-6">
